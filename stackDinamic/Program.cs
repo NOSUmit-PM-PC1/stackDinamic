b5d4c6417bb0dfd623830f8e064c3096bbdb3209
Мария <mahadrum@yandex.ru>
2021-05-20 10:38:23 +0300

    Добавьте файлы проекта.

---
+++ stackDinamic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackDinamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DinElement a = new DinElement(3);
+            Console.WriteLine(a);
+
+            DinElement b = new DinElement();
+            b.Value = 45;
+            Console.WriteLine(b);
+
+            DinStack st = new DinStack();
+            st.Push(3);
+            st.Push(5);
+            st.Push(1);
+
+            Console.WriteLine(st);
+
+            Console.WriteLine(st.Peek());
+        }
+    }
+}
---
+++ stackDinamic/dinamicStructure.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackDinamic
+{
+    class DinElement
+    {
+        int val;
+        DinElement adr;
+
+        public int Value 
+        {
+            get { return val;}
+            set { val = value; }
+            //set { }
+        }
+
+        internal DinElement Adress
+        {
+            get { return adr; }
+            set { adr = value; }
+        }
+
+        public DinElement(int a)
+        {
+            val = a;
+            adr = null;
+        }
+
+        public DinElement()
+        {
+            val = 0;
+            adr = null;
+        }
+
+        public override string ToString()
+        {
+            return "value = " + val;
+        }
+    }
+
+    class DinStack
+    {
+        DinElement top = null;
+
+        public void Push(int value)
+        {
+            DinElement el = new DinElement(value);
+            el.Adress = top;
+            top = el;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            DinElement temp = top;
+            while (temp != null)
+            {
+                s += temp.Value + " ";
+                temp = temp.Adress;
+            }
+            return s;
+            //top.Value + " " + top.Adress.Value + " " + top.Adress.Adress.Value;
+        }
+
+        public bool isEmpty()
+        {
+            return top == null;
+        }
+
+        public int Peek()
+        {
+            if (isEmpty())
+            {
+                throw new Exception("Стек пустой!");
+            }
+            else
+            {
+                return top.Value;
+            }
+        }
+
+        public int Pop()
+        {
+            if (isEmpty())
+            {
+                throw new Exception("Стек пустой!");
+            }
+            else
+            {
+                int temp = top.Value;
+                top = top.Adress;
+                return temp;
+            }
+        }
+    }
+}
